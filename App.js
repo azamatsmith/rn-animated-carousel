8100a4c808d663fed63338c6061c3c04848e5566
Matthew Smith <azamatsmith@gmail.com>
2020-07-26 14:43:38 -0700

    Code cleanup

--- App.js
+++ App.js
@@ -1,20 +1,8 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { AppLoading } from 'expo';
 import Main from './src/screens/Home';
 
-// https://github.com/expo/google-fonts/tree/master/font-packages/roboto
-import { useFonts, Roboto_400Regular } from '@expo-google-fonts/roboto';
-
 function App() {
-  let [fontsLoaded] = useFonts({
-    Roboto_400Regular,
-  });
-
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  }
-
   return (
     <>
       <StatusBar hidden />
