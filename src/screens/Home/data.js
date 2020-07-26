18bc2f5e670004c8b0e7c6768c8ab316d47268da
Matthew Smith <azamatsmith@users.noreply.github.com>
2020-07-26 11:01:28 -0700

    Feature - Image Carousel (#1)

--- src/screens/Home/data.js
+++ src/screens/Home/data.js
@@ -1,9 +1,34 @@
 export default [
   {
+    key: 0,
     color: '#ddd',
     image: require('./images/r6_front.png'),
     imageTitle: 'Game changer',
     title: 'EOS R6',
-    subTitle: `The EOS R6 full-frame mirrorless camera is designed to meet the demands of both photographers and video content creators who want a strong and versatile camera to keep up with their active lives.`,
+    description: `The EOS R6 full-frame mirrorless camera is designed to meet the demands of both photographers and video content creators who want a strong and versatile camera to keep up with their active lives.`,
+  },
+  {
+    key: 1,
+    color: '#ddd',
+    image: require('./images/r6_back.png'),
+    imageTitle: 'Game changer',
+    title: 'EOS R6',
+    description: `The EOS R6 full-frame mirrorless camera is designed to meet the demands of both photographers and video content creators who want a strong and versatile camera to keep up with their active lives.`,
+  },
+  {
+    key: 2,
+    color: '#ddd',
+    image: require('./images/r6_top.png'),
+    imageTitle: 'Game changer',
+    title: 'EOS R6',
+    description: `The EOS R6 full-frame mirrorless camera is designed to meet the demands of both photographers and video content creators who want a strong and versatile camera to keep up with their active lives.`,
+  },
+  {
+    key: 3,
+    color: '#ddd',
+    image: require('./images/r6_front_sensor.png'),
+    imageTitle: 'Game changer',
+    title: 'EOS R6',
+    description: `The EOS R6 full-frame mirrorless camera is designed to meet the demands of both photographers and video content creators who want a strong and versatile camera to keep up with their active lives.`,
   },
 ];
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,24 +1,43 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import styled from 'styled-components/native';
+import { Animated, Dimensions } from 'react-native';
 
-import ImageList from './components/ImageList';
+import Page from './components/Page';
 import Pagination from './components/Pagination';
 import ScrollTitle from './components/ScrollTitle';
 import SideText from './components/SideText';
 
-// import data from './data';
+import data from './data';
 
 const StyledHome = styled.View`
   flex: 1;
 `;
 
+const WIDTH = Dimensions.get('window').width;
+// const HEIGHT = Dimensions.get('window').height;
+
 function Home() {
+  const scrollX = React.useRef(new Animated.Value(0)).current;
   return (
     <StyledHome>
       <SideText />
-      <ImageList />
       <ScrollTitle />
+      <Animated.FlatList
+        data={data}
+        renderItem={({ item, index }) => (
+          <Page index={index} item={item} scrollX={scrollX} width={WIDTH} />
+        )}
+        keyExtractor={(item) => item.key.toString()}
+        horizontal
+        pagingEnabled
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+          { useNativeDriver: true }
+        )}
+        scrollEventThrottle={16}
+        showsHorizontalScrollIndicator={false}
+      />
       <Pagination />
     </StyledHome>
   );
